18e38531171d196a5693f22495b30a2b7f81ead4
Mashood Ali <mashoodali219@gmail.com>
2023-07-21 03:21:58 +0500

    setup the mvc file structure

--- server/server.js
+++ server/server.js
@@ -1,40 +1,17 @@
 import express from "express";
-import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
-const app = express();
-
-// app.use(bodyParser.json({ limit: "30mb", extended: true }));
-// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-// app.use(cors());
-
-// const CONNECTION_URL =
-//   "mongodb+srv://mashood:boom@node-projects.lvpwy1s.mongodb.net/Memory-Project?retryWrites=true&w=majority";
-// const PORT = process.env.PORT || 4000;
-
-// mongoose
-//   .connect(CONNECTION_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     app.listen(PORT, () => {
-//       console.log(`server running on Port: ${PORT}`);
-//     })
-//   )
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// mongoose.set("useFindAndModify", false);
-
-// database connection
-// const connectDB = require("./db/connect");
 import connectDB from "./db/db.js";
-// environement variables
-
+import postRoute from "./routes/post.js";
 import dotenv from "dotenv";
 dotenv.config();
+
+const app = express();
+
+app.use("/post", postRoute);
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 // connection string
 const connectionString = process.env.MONGO_URI;
