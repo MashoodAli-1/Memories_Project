4d3435acd1a5dc8c8ef61617e5f3c5ffa5849e84
Mashood Ali <mashoodali219@gmail.com>
2023-07-22 01:10:39 +0500

    add style to App.js and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
+import useStyles from "./styles";
 import { Container, AppBar, Typography, Grow, Grid } from "@mui/material";
-
+import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 function App() {
+  const classes = useStyles();
   return (
     <>
       <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-          <Typography variant="h2" align="center">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
             Memories
+            <img
+              className={classes.image}
+              src={memories}
+              alt="memory logo"
+              height={60}
+            />
           </Typography>
         </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems={"stretch"}
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
       </Container>
     </>
   );
